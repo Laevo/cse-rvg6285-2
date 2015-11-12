f6bbae3e9f12777f7d3545f2861bb16ef6faf1a2
Laevo <rohitvgaikwad@mavs.uta.edu>
2015-11-12 00:51:07 -0600

    table added

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require "redis"
 # Inserting into Postgres
 # heroku run rake db:seed --app cse-rvg6285-2
 
-pgfile = File.read('/home/laevo/cse-rvg6285-2/public/pgdata.json')
+pgfile = File.read('pgdata.json')
 pg_data = JSON.parse(pgfile)
 pg_data.each do |record|
   Population.create!(record)
@@ -12,7 +12,7 @@ pg_data.each do |record|
 
 # Inserting into Redis
 redis = Redis.new
-redisfile = File.read('/home/laevo/cse-rvg6285-2/public/redisdata.json')
+redisfile = File.read('redisdata.json')
 redis_data = JSON.parse(redisfile)
 redis_data.each do |key, value|
    redis.set("#{key}", "#{value}")  
