0723669f6677b69cf62ec6dd178534d0b8911b70
Laevo <rohitvgaikwad@mavs.uta.edu>
2015-11-12 06:24:43 -0600

    asd

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
 require "redis"
 require 'mongo'
-# Inserting into Postgres
-# heroku run rake db:seed --app cse-rvg6285-2
+# # Inserting into Postgres
+# # heroku run rake db:seed --app cse-rvg6285-2
 
-pgfile = File.read('pgdata.json')
-pg_data = JSON.parse(pgfile)
-pg_data.each do |record|
-  Population.create!(record)
-  end
+# pgfile = File.read('pgdata.json')
+# pg_data = JSON.parse(pgfile)
+# pg_data.each do |record|
+#   Population.create!(record)
+#   end
 
 
-# # # Inserting into Redis
-redis = Redis.new
-redisfile = File.read('redisdata.json')
-redis_data = JSON.parse(redisfile)
-redis_data.each do |key, value|
-   redis.set("#{key}", "#{value}")  
-  end
+# # # # Inserting into Redis
+# redis = Redis.new
+# redisfile = File.read('redisdata.json')
+# redis_data = JSON.parse(redisfile)
+# redis_data.each do |key, value|
+#    redis.set("#{key}", "#{value}")  
+#   end
 
 # Inserting into mongo
 mongo_uri = ENV['mongodb://heroku_kkpqddm8:41chrq1pn46pqql09h3nmut8u1@ds053764.mongolab.com:53764/heroku_kkpqddm8']
@@ -25,5 +25,5 @@ db = client.heroku_kkpqddm8
 mongofile = File.read('redisdata.json')
 mongo_data = JSON.parse(mongofile)
  mongo_data.each do |key, value|
-     db[:population].insert_one({_id: "#{key}", population:"#{value}"})
+     db[:populatio].insert_one({_id: "#{key}", population:"#{value}"})
  end
