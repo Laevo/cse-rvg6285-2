415da355b0ef0bddfc62a993eb40f2f7e5b1a082
Laevo <rohitvgaikwad@mavs.uta.edu>
2015-11-12 06:19:44 -0600

    asd

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "redis"
-
+require 'mongo'
 # Inserting into Postgres
 # heroku run rake db:seed --app cse-rvg6285-2
 
@@ -10,7 +10,7 @@ pg_data.each do |record|
   end
 
 
-# Inserting into Redis
+# # # Inserting into Redis
 redis = Redis.new
 redisfile = File.read('redisdata.json')
 redis_data = JSON.parse(redisfile)
@@ -18,3 +18,12 @@ redis_data.each do |key, value|
    redis.set("#{key}", "#{value}")  
   end
 
+# Inserting into mongo
+mongo_uri = ENV['mongodb://heroku_kkpqddm8:1chrq1pn46pqql09h3nmut8u1@ds053764.mongolab.com:53764/heroku_kkpqddm8']
+client = Mongo::Client.new(mongo_uri)
+db = client.heroku_kkpqddm8
+mongofile = File.read('redisdata.json')
+mongo_data = JSON.parse(mongofile)
+ mongo_data.each do |key, value|
+     db[:population].insert_one({_id: "#{key}", population:"#{value}"})
+ end
--- lib/tasks/pg_load.rake
+++ lib/tasks/pg_load.rake
@@ -2,14 +2,10 @@ task :pg_load do
 require 'rubygems'
 require 'json'
 
-  # Create the connection instance.
 
-data_file = File.read('/home/laevo/cse-rvg6285-2/public/pgdata.json')
-data_parse = JSON.parse(data_file)
-# for (i=0; i<data_parse.counters.length; i++){
-# 	puts data_parse['Country'.to_i]
-#  }
-data_parse.each do |record|
-   Population.create!(record)
-end
+pgfile = File.read('/home/laevo/cse-rvg6285-2/public/pgdata.json')
+pg_data = JSON.parse(pgfile)
+pg_data.each do |record|
+  Population.create!(record)
+  end
 end
