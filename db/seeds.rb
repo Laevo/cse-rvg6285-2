14137a9bddeba8acac854ec9e48db75a7ee9b2c5
Laevo <rohitvgaikwad@mavs.uta.edu>
2015-11-13 19:23:57 -0600

    Mongo connection

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,20 @@ require 'mongo'
 
 # # # # Inserting into Redis
 # redis = Redis.new
-# redisfile = File.read('redisdata.json')
+# redisfile = File.read('redismongo.json')
 # redis_data = JSON.parse(redisfile)
 # redis_data.each do |key, value|
 #    redis.set("#{key}", "#{value}")  
 #   end
 
 # Inserting into mongo
-mongo_uri = ENV['mongodb://heroku_kkpqddm8:41chrq1pn46pqql09h3nmut8u1@ds053764.mongolab.com:53764/heroku_kkpqddm8']
-client = Mongo::Client.new(mongo_uri)
+mongo_uri = ENV['MONGOLAB_URI']
+client = Mongo::Client.new(mongo_uri);
 db = client.database
-mongofile = File.read('redisdata.json')
+
+mongofile = File.read('redismongo.json')
 mongo_data = JSON.parse(mongofile)
- # mongo_data.each do |key, value|
- # 	puts "#{key}"
+  mongo_data.each do |key, value|
+  	puts "#{key}"
      # db[:populatio].insert_one({_id: "#{key}", population:"#{value}"})
  #end
